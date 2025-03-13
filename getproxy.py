91e4c80152642db06b3d466ce4f88a9cf1a970fa
eldepor <mracing@gmail.com>
2025-03-13 23:31:17 +0100

    Update getproxy.py

--- getproxy.py
+++ getproxy.py
@@ -153,7 +153,7 @@ class Downloadproxies():
 
     def get(self):
         self.proxy_dict['socks4'] += self.get_special1()
-        self.get_special2()
+        #self.get_special2()
         #self.get_extra()
         
         for type in ['socks4','socks5','http']:
